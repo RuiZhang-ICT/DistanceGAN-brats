3426fbcc7067d2f33bdd310188e49562b7f18bd8
RuiZhang-ICT <zhangrui@ict.ac.cn>
2018-09-04 06:31:30 +0000

    test

--- scripts/test_Flair_T1.sh
+++ scripts/test_Flair_T1.sh
@@ -1,1 +1,1 @@
-python test.py --dataroot ../datasets/BRATS_images --dataA Synthetic_Flair_test --dataB Synthetic_T1_test --name distancegan_brats_flair_t1 --model distance_gan --input_nc 1 --output_nc 1 --phase test 
+python test.py --dataroot ../datasets/BRATS_images --dataA Synthetic_Flair_test --dataB Synthetic_T1_test --name distancegan_brats_flair_t1 --model distance_gan --input_nc 1 --output_nc 1 --phase test --loadSize 256
